39bbe2606bb5f2f61b370aa77f2e8a6a91cca8a6
Vilen <liongrig434@gmail.com>
2024-11-12 23:27:41 +0300

    Добавил модификатор доступа private к элементам

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,36 +1,28 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Вилен Дашкин on 11.11.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var changeButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var historyText: UITextView!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var changeButton: UIButton!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var historyText: UITextView!
     private var counter: Int = 0
     private var textView: String = " "
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         plusButton.tintColor = .red
         minusButton.tintColor = .blue
-
+        
     }
-    @IBAction func buttonDidTap(_ sender: Any) {
+    @IBAction private func buttonDidTap(_ sender: Any) {
         counter = 0
         counterLabel.text = String(counter)
         let newEntry: String = "Значение сброшено"
         historyText.text += "\n[\(Date())] \(newEntry)"
     }
     
-    @IBAction func buttonDidPlus(_ sender: Any) {
+    @IBAction private func buttonDidPlus(_ sender: Any) {
         if counter <= 0{
             counter = 0
             counter += 1
@@ -40,11 +32,11 @@ class ViewController: UIViewController {
         counterLabel.text = String(counter)
         let newEntry: String = "Значение изменено на +1"
         historyText.text += "\n[\(Date())] \(newEntry)"
-       
+        
         
     }
     
-    @IBAction func buttonDidMinus(_ sender: Any) {
+    @IBAction private func buttonDidMinus(_ sender: Any) {
         counter -= 1
         if counter >= 0{
             counterLabel.text = String(counter)
@@ -57,8 +49,6 @@ class ViewController: UIViewController {
         }
         
     }
-    
-    
     
     
 }
